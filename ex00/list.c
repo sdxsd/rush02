ca834aad4013d1b854f6891fea5b7b84037be079
Job van der Zweep <jobvan-d@student.codam.nl>
2021-07-24 17:15:04 +0200

    list!

---
+++ ex00/list.c
@@ -0,0 +1,76 @@
+#include "list.h"
+
+#include <stdlib.h>
+
+/* TODO: REMOVE STDIO HEADER, REMOVE/PORT LI_DISPLAY */
+#include <stdio.h>
+
+/* adds new elem with key and value to the list.
+ * If li is null, creates the list. Notice that t_list is 
+ * a double pointer, because othewise you wouldn't be able
+ * to create the list.
+ * also DOES NOT check if malloc returns NULL pointer */
+void	li_create_append(t_list **li, long key, char *value)
+{
+	t_list	*n;
+	t_list	*cur;
+
+	n = malloc(sizeof(t_list));
+	n->key = key;
+	n->value = value;
+	n->next = (t_list *)0;
+	if (*li == (t_list *)0)
+	{
+		*li = n;
+		return ;
+	}
+	cur = *li;
+	while (cur->next != (t_list *)0)
+	{
+		cur = cur->next;
+	}
+	cur->next = n;
+}
+
+/* frees the entire list, including the value strings. */
+/* TODO: fix*/
+void	li_free(t_list *li)
+{
+	t_list	*cur;
+	t_list	*t;
+
+	cur = li;
+	while (cur != (t_list *)0)
+	{
+		t = cur->next;
+		free(cur->value);
+		free(cur);
+		cur = t;
+	}
+}
+
+/* walks over the list and runs func for every element. */
+void	li_foreach(t_list *li, void (*func)(t_list *li))
+{
+	t_list	*cur;
+
+	cur = li;
+	while (cur != (t_list *)0)
+	{
+		func(cur);
+		cur = cur->next;
+	}
+}
+
+/* displays individual list  element. cannot be called outside
+ * of this file because it's not included in list.h */
+void	li_display_element(t_list *li)
+{
+	printf("%p: %ld: %s\n", li, li->key, li->value);
+}
+
+/* prints list to stdout */
+void	li_display(t_list *li)
+{
+	li_foreach(li, &li_display_element);
+}
---
+++ ex00/list.h
@@ -0,0 +1,23 @@
+#ifndef LIST_H
+#define LIST_H
+
+typedef struct	s_list
+{
+	struct	s_list	*next;
+	long			key;
+	char			*value;
+} t_list;
+
+/* adds new elem with key and value to the list. if li is null, creates the list */
+void	li_create_append(t_list **li, long key, char *value);
+
+/* frees the entire list, including the value strings */
+void	li_free(t_list *li);
+
+/* walks over the list and runs func for every element */
+void	li_foreach(t_list *li, void (*func)(t_list *li));
+
+/* prints list to stdout */
+void	li_display(t_list *li);
+
+#endif /* LIST_H */
