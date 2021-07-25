b3dd79c63d01053fe53fd9c2fc09c35426645354
Job van der Zweep <jobvan-d@student.codam.nl>
2021-07-25 13:47:33 +0200

    remove unused ft_atoi from lib.c

--- ex00/lib.c
+++ ex00/lib.c
@@ -74,37 +74,3 @@ void	ft_putnbr(long nb)
 		ft_putchar((nb % 10) + '0');
 	}
 }
-
-/* TODO: whitespace /t/v etc? */
-long ft_atoi(const char *str)
-{
-    long	iterator;
-    long	minus_count;
-    long	plus_count;
-    long	nbr;
-
-    nbr = 0;
-    iterator = 0;
-    minus_count = 0;
-    plus_count = 0;
-    while (str[iterator] != '\0')
-    {
-        if (str[iterator] == ' ')
-            continue;
-        if (str[iterator] == '-')
-            ++minus_count;
-        else if (str[iterator] == '+')
-            ++plus_count;
-        else if (str[iterator] >= '0' && str[iterator] <= '9')
-        {
-            nbr *= 10;
-            nbr += (long)str[iterator] - '0';
-        }
-        else
-            break;
-        ++iterator;
-    }
-    if (minus_count % 2 == 1)
-        nbr = -nbr;
-    return (nbr);
-}
