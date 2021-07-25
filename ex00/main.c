a930ede364e6807d3d8a2535c3fd3e6da85d631d
Job van der Zweep <jobvan-d@student.codam.nl>
2021-07-25 20:32:53 +0200

    maybe

--- ex00/main.c
+++ ex00/main.c
@@ -1,24 +1,15 @@
 #include <unistd.h>
 #include "list.h"
+//#include "lib.h"
+//#include "ft_atoi.h"
 
-int	ft_check_len(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (*str)
-	{
-		i++;
-		str++;
-	}
-	return (i);
-}
+long g_num_to_check;
 
 int	ft_check(char *str)
 {
 	int	len;
 
-	len = ft_check_len(str);
+	len = ft_strlen(str);
 	while (*str)
 	{
 		if (!(*str >= '0' && *str <= '9'))
@@ -38,6 +29,15 @@ int	ft_check(char *str)
 
 int	parse_file(char	*filename, t_list **list);
 
+void	print_if_eq(t_list *li)
+{
+	if (li->key == g_num_to_check)
+	{
+		ft_putstr(li->value);
+		ft_putchar('\n');
+	}
+}
+
 int	main(int argc, char **argv)
 {
 	t_list	*li	= (t_list *)0;
@@ -48,7 +48,9 @@ int	main(int argc, char **argv)
 		return (1);
 	}
 	ft_check(argv[1]);
+	g_num_to_check = ft_atoi(argv[1]);
 	parse_file("numbers.dict", &li);
-	li_display(li);
+	//li_display(li);
+	li_foreach(li, &print_if_eq); 
 	return (0);
 }
