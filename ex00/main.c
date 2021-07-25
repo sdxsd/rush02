1037a9c6e7bb10f94173b5b877cabaa668ded0cb
Job van der Zweep <jobvan-d@student.codam.nl>
2021-07-25 20:51:14 +0200

    slight improvement

--- ex00/main.c
+++ ex00/main.c
@@ -34,12 +34,13 @@ void	print_if_eq(t_list *li)
 	if (li->key == g_num_to_check)
 	{
 		ft_putstr(li->value);
-		ft_putchar('\n');
+		ft_putchar(' ');
 	}
 }
 
 int	main(int argc, char **argv)
 {
+	long	tmp;
 	t_list	*li	= (t_list *)0;
 
 	if (argc != 2)
@@ -49,8 +50,19 @@ int	main(int argc, char **argv)
 	}
 	ft_check(argv[1]);
 	g_num_to_check = ft_atoi(argv[1]);
+	tmp = g_num_to_check;
 	parse_file("numbers.dict", &li);
-	//li_display(li);
-	li_foreach(li, &print_if_eq); 
+	if (g_num_to_check >= 20 && g_num_to_check % 10 != 0)
+	{
+		g_num_to_check = (tmp / 10) * 10;
+		li_foreach(li, &print_if_eq);
+		g_num_to_check = tmp % 10;
+		li_foreach(li, &print_if_eq);
+	}
+	else
+	{
+		li_foreach(li, &print_if_eq);
+	}
+	ft_putchar('\n');
 	return (0);
 }
