c0c1744e0242c560cfd059f738e3b3ea17f2eb69
Job van der Zweep <jobvan-d@student.codam.nl>
2021-07-25 18:59:30 +0200

    don't need this lol

--- ex00/lib.h
+++ ex00/lib.h
@@ -60,24 +60,4 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-void	ft_putnbr(long nb)
-{
-	if (nb < 0)
-	{
-		ft_putchar('-');
-		nb = -nb;
-	}
-	if (nb < 10)
-	{
-		ft_putchar(nb + '0');
-	}
-	else
-	{
-		ft_putnbr(nb / 10);
-		ft_putchar((nb % 10) + '0');
-	}
-}
-
 #endif /* LIB_H */
-
-
