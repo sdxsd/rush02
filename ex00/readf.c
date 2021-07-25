ad773aa561e2f2ed8544187a4cc7eaf62caa5a5b
Job van der Zweep <jobvan-d@student.codam.nl>
2021-07-25 20:16:32 +0200

    readf

--- ex00/readf.c
+++ ex00/readf.c
@@ -1,61 +1,100 @@
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include "lib.h"
 #include "ft_atoi.h"
+#include "list.h"
 
-int fgetline()
-{
-    open("numbers.dict", O_RDONLY);
-    char buffer[8096];
-    int file = open("numbers.dict", O_RDONLY);
-    if (file == -1)
-    {
-        write(1, "error\n", 6);
-        return (1);
-    }
-    read(file, &buffer[0], sizeof(buffer));
-    close(file);
-    ft_putstr(buffer);
-    return (0);
+#include <stdio.h>
+
+#define BUF_SIZE (4096)
+
+long	m_parse_line(char *line, t_list **list);
+char	*m_getline(int file);
+
+int	parse_file(char	*filename, t_list **list)
+{	
+	int		file;
+	char	*line;
+	long	prev;
+	long	cur;
+
+	prev = -2478289893455;
+	file = open(filename, O_RDONLY);
+	if (file == -1)
+	{
+		return (-1);
+	}
+	while (1)
+	{
+		line = m_getline(file);
+		cur = m_parse_line(line, list);
+		free(line);
+		if (cur == prev)
+		{
+			return (1);
+		}
+		prev = cur;
+	}
+	return (1);
 }
 
-int get_entry(char *file_path, int lines_to_skip)
+long	m_parse_line(char *line, t_list **list)
 {
-    char char_buf[4096];
-    char entry_buf[4096];
-    char one_char;
-    int file;
-    int iterator;
-    int iterator2;
-    int nl_count;
-    
-    nl_count = 0;
-    iterator = 0;
-    file = open(file_path, O_RDONLY);
-    while ((read(file, &one_char, 1)) && one_char != '\0')
-    {
-        char_buf[iterator] = one_char;
-        ++iterator;
-    }
-    iterator = 0;
-    iterator2 = 0;
-    while (iterator < 4096)
-    {
-        if (char_buf[iterator] == '\n')
-            ++nl_count;
-        else if (nl_count == lines_to_skip)
-            entry_buf[iterator2] = char_buf[iterator];
-        iterator++;
+	long	key;
+	char	*value;
+	int		len;
+	int		i;
 
-    }
-    char_buf[iterator] = '\0';
-    ft_putstr(entry_buf);
-    close (file);
-    return(0);
+	len = ft_strlen(line);
+	if (len <= 3)
+	{
+		return (0);
+	}
+	//printf("%s\n", line);
+	key = ft_atoi(line);
+	i = 0;
+	while (line[i] != ':')
+	{
+		i++;
+	}
+	i++;
+	value = malloc(len - i);
+	ft_strcpy(value, (line + i));
+	li_create_append(list, key, value);
+	return key;
 }
+
+/* reads a line of at most BUF_SIZE in length, returns char* 
+ * IT SHOULD BE FREED. */
+char	*m_getline(int file)
+{
+	char	buf[BUF_SIZE];
+	char	*line;
+	int		i;
+
+	i = 0;
+	while (1)
+	{
+		read(file, buf + i, 1);
+		if (buf[i] == '\n' || buf[i] == 0 || i >= BUF_SIZE)
+		{
+			line = malloc(i + 1);
+			ft_strcpy(line, buf);
+			line[i] = 0;
+			return (line);
+		}
+		i++;
+	}
+}
+
 
 int main()
 {
-    //fgetline();
-    get_entry("numbers.dict", 4);
+	t_list	*li = (t_list *)0;
+	parse_file("numbers.dict", &li);
+	li_display(li);
+	return (0);
 }
+
+#undef BUF_SIZE
